668d4d11cbc9f1efa62445f2d4448377095b0227
XIAORAN XIE <59595778+Makiato1999@users.noreply.github.com>
2023-02-20 01:49:48 -0600

    Updated
    Updated

--- 15. The Simon Game/game.js
+++ 15. The Simon Game/game.js
@@ -3,36 +3,7 @@ var randomChosenColour;
 var gamePattern = [];
 var userClickedPattern = [];
 var ifStarted = false;
-
-function nextSequence() {
-    let randomNumber = Math.floor(Math.random() * 4);
-    randomChosenColour = buttonColours[randomNumber]
-    gamePattern.push(randomChosenColour);
-
-    // flash the relevent color button
-    let className = "#"+randomChosenColour;
-    console.log(className);
-    $(className).animate({
-        opacity: 0,
-    }, 500).animate({
-        opacity: 1
-    }, 500);
-
-    playSound(randomChosenColour);
-}
-
-$(".btn").click(function (e) { 
-    let clickedElement = e.target;// which includes class, id, etc.
-    let userChosenColour = clickedElement.id;
-    playSound(userChosenColour);
-    userClickedPattern.push(userChosenColour);
-});
-
-// play sounds
-function playSound(name) {
-    var audio = new Audio('sounds/'+name+'.mp3');
-    audio.play();
-}
+var level = 0;
 
 $(document).keypress(function() {
     if (!ifStarted) {
@@ -40,3 +11,41 @@ $(document).keypress(function() {
         nextSequence();
     }
 });
+
+function nextSequence() {
+    let randomNumber = Math.floor(Math.random() * 4);
+    randomChosenColour = buttonColours[randomNumber]
+    gamePattern.push(randomChosenColour);
+    // flash the relevent color button
+    let id = "#"+randomChosenColour;
+    $(id).animate({
+        opacity: 0,
+    }, 500).animate({
+        opacity: 1
+    }, 500);
+    playSound(randomChosenColour);
+}
+
+$(".btn").click(function (e) { 
+    let clickedElement = e.target;// which includes class, id, etc.
+    let userChosenColour = clickedElement.id;
+    userClickedPattern.push(userChosenColour);
+
+    playSound(userChosenColour);
+
+    animatePress(userChosenColour);
+});
+
+// play sounds
+function playSound(name) {
+    var audio = new Audio('sounds/'+name+'.mp3');
+    audio.play();
+}
+// press button animate
+function animatePress(currentColour) {
+    let id = "#"+currentColour;
+    $(id).addClass("pressed");
+    setTimeout(() => {
+        $(id).removeClass("pressed");
+    }, 100);
+}
