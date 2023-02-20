441236f7389fab89823d8b43aa3c9cc45c2f43c2
XIAORAN XIE <59595778+Makiato1999@users.noreply.github.com>
2023-02-19 22:13:20 -0600

    Updated
    Updated

--- 15. The Simon Game/game.js
+++ 15. The Simon Game/game.js
@@ -2,60 +2,25 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 var randomChosenColour;
 var gamePattern = [];
 var userClickedPattern = [];
-
+var ifStarted = false;
 
 function nextSequence() {
     let randomNumber = Math.floor(Math.random() * 4);
     randomChosenColour = buttonColours[randomNumber]
-    // playSound(randomChosenColour);
     gamePattern.push(randomChosenColour);
+
+    // flash the relevent color button
+    let className = "#"+randomChosenColour;
+    console.log(className);
+    $(className).animate({
+        opacity: 0,
+    }, 500).animate({
+        opacity: 1
+    }, 500);
+
+    playSound(randomChosenColour);
 }
 
-nextSequence();
-nextSequence();
-nextSequence();
-nextSequence();
-
-console.log(gamePattern);
-/*
-var ifLocked = true;
-// flash the relevent color button
-for (let i = 0; i < gamePattern.length; i++) {
-    let className = "."+gamePattern[i];
-    if (ifLocked) {
-        ifLocked = false;
-        console.log(className);
-        $(className).animate({
-            opacity: 0,
-        }, 500).animate({
-            opacity: 1
-        }, 500);
-        setTimeout(() => {
-            ifLocked = true;
-        }, 1000);
-    }
-}*/
-var ifLocked = true;
-let counter = 0;
-while (counter < gamePattern.length) {
-    console.log(counter);
-    let className = "."+gamePattern[counter];
-    if (ifLocked) {
-        ifLocked = false;
-        console.log(className);
-        $(className).animate({
-            opacity: 0,
-        }, 500).animate({
-            opacity: 1
-        }, 500);
-        setTimeout(() => {
-            counter ++;
-            ifLocked = true;
-        }, 1000);
-    }
-}
-
-// play sounds
 $(".btn").click(function (e) { 
     let clickedElement = e.target;// which includes class, id, etc.
     let userChosenColour = clickedElement.id;
@@ -63,7 +28,15 @@ $(".btn").click(function (e) {
     userClickedPattern.push(userChosenColour);
 });
 
+// play sounds
 function playSound(name) {
     var audio = new Audio('sounds/'+name+'.mp3');
     audio.play();
 }
+
+$(document).keypress(function() {
+    if (!ifStarted) {
+        ifStarted = true;
+        nextSequence();
+    }
+});
