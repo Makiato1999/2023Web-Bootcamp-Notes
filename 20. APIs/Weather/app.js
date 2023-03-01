b2bfa79d48c3811e8ab05ce4635ca61ba2c3ec47
XIAORAN XIE <59595778+Makiato1999@users.noreply.github.com>
2023-03-01 01:45:57 -0600

    update
    update

--- 20. APIs/Weather/app.js
+++ 20. APIs/Weather/app.js
@@ -6,7 +6,19 @@ const app = express();
 app.get("/", function(req, res) {
     const url = "https://api.openweathermap.org/data/2.5/weather?q=Winnipeg&appid=cdbbc9c6e2b482d21f3c811286c677d3";
     https.get(url, function(response) {
-        console.log(response);
+        console.log("statusCode: "+response.statusCode);
+        console.log("headers: "+response.headers);
+        response.on("data", (data)=>{
+            const weatherData = JSON.parse(data);
+            const object = {
+                name: "Shawn",
+                age: 23
+            }
+            console.log(JSON.stringify(object));
+            console.log(weatherData.main.temp);
+            const weatherDesciption = weatherData.weather[0].description;
+            console.log(weatherDesciption);
+        });
     });
     res.send("Server is up and running");
 })
