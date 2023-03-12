66e88169abb5694db04cf6e21e929c903b215d30
XIAORAN XIE <59595778+Makiato1999@users.noreply.github.com>
2023-03-12 00:14:23 -0600

    update
    update

--- 22. EJS/todolist-v1/app.js
+++ 22. EJS/todolist-v1/app.js
@@ -6,38 +6,27 @@ const app = express();
 
 app.set("view engine", "ejs");
 
+app.use(bodyParse.urlencoded({extended:true}));
+
 app.get("/", (req, res)=>{
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
 
-    switch (currentDay) {
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;
-        default:
-            console.log("Error: current day is equal to: "+dayKind);
-    }
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+    var day = today.toLocaleDateString("en-US", options);
 
-    res.render("list", {kindOfDay: day});
+    res.render("list", {
+        kindOfDay: day
+    });
+    // res.render("list", {kindOfDay: day});
+});
+
+app.post("/", (req, res)=>{
+    var item = req.body.newItem;
+    console.log(item);
 });
 
 app.listen(3000, ()=>{
