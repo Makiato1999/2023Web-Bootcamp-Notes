aa1f9eb33850726ba5e72a0e2411625777725b56
XIAORAN XIE <59595778+Makiato1999@users.noreply.github.com>
2023-03-27 00:57:03 -0500

    update
    update

--- 28. Putting Everything Together/todolist-v2/app.js
+++ 28. Putting Everything Together/todolist-v2/app.js
@@ -1,44 +1,60 @@
 // jshint esversion:6
 const express = require("express");
 const bodyParse = require("body-parser");
+/* we temporarily don't consider the date in todolist-v1
 const getDate = require("./date");
-const mongoose = require("mongoose");
-// import module
 const date = require(__dirname+"/date.js");
-
+let day = date.getDay();
+*/
+const mongoose = require("mongoose");
 const app = express();
 
 app.set("view engine", "ejs");
-
 app.use(bodyParse.urlencoded({extended:true}));
 app.use(express.static("public"));
 
-let items = [
-    "don't forget breakfast",
-    "learn Udemy",
-    "coffee time and take a nap"
-];
+
+// initialize the database
+const itemSchema = new mongoose.Schema({
+    item: String
+});
+const Item = mongoose.model("Item", itemSchema);
+const item_1 = new Item({
+    item: "write anything you would like to do"
+});
+const item_2 = new Item({
+    item: "click on + button to add a new item"
+});
+const item_3 = new Item({
+    item: "<-- click on this checkbox to delete the item"
+});
+const defaultitems = [item_1, item_2, item_3];
+let items = [];
 let workItems = [];
 
+main().catch(err => console.log(err));
+async function main() {
+    try {
+        await mongoose.connect('mongodb://localhost:27017/todolistDB', {useNewUrlParser: true});
+        items = await Item.insertMany(defaultitems);
+    } catch (error) {
+        console.error(error);
+    }
+    console.log(items);
+
+    try {
+        await mongoose.connection.close();
+        console.log('Mongoose connection closed.');
+    } catch (error) {
+        console.error('Error closing Mongoose connection:', error);
+    }
+}
+
 app.get("/", (req, res)=>{
-    /* put this part into data.js
-    let today = new Date();
-
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-    let day = today.toLocaleDateString("en-US", options);
-    */
-    //let day = getDate();
-    let day = date.getDay();
-
     res.render("list", {
-        listTitle: day,
+        listTitle: "Today",
         newItems: items
     });
-    // res.render("list", {kindOfDay: day});
 });
 
 app.post("/", (req, res)=>{
