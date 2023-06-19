d4f16d59b5e7722485f7954c1376a8fe4716d7ca
XIAORAN XIE <59595778+Makiato1999@users.noreply.github.com>
2023-06-19 13:43:59 +0800

    update

--- 38. React.js/react-styling-practice/src/index.js
+++ 38. React.js/react-styling-practice/src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+//import PI, { doublePi, triplePi } from "./math.js";
+import * as PI from "./PI.js";
+import * as Calculator from "./math";
+// import { add, subtract, mutiply, divide } from "./math.js";
 
 const date = new Date(2022, 1, 1, 19);
 const currentTime = date.getHours();
@@ -8,6 +12,8 @@ let greeting;
 const customStyle = {
   color: ""
 };
+let a = 6;
+let b = 3;
 
 if (currentTime < 12) {
   greeting = "Good Morning";
@@ -26,7 +32,20 @@ ReactDOM.render(
       {greeting}
     </h1>
     <App />
-    <App />
+    <h3>PI</h3>
+    <ul>
+      <li>{PI.doublePi()}</li>
+      <li>{PI.triplePi()}</li>
+    </ul>
+    <h3>
+      When a = {a}, b = {b}
+    </h3>
+    <ul>
+      <li>a+b = {Calculator.add(a, b)}</li>
+      <li>a-b = {Calculator.subtract(a, b)}</li>
+      <li>a*b = {Calculator.mutiply(a, b)}</li>
+      <li>a/b = {Calculator.divide(a, b)}</li>
+    </ul>
   </div>,
   document.getElementById("root")
 );
