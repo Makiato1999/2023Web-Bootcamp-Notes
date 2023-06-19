aee01394ed78eb154b6033e2f32984c7dd3228df
XIAORAN XIE <59595778+Makiato1999@users.noreply.github.com>
2023-06-19 10:40:31 +0800

    update

---
+++ 38. React.js/react-styling-practice/src/components/List.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li>Apple Vison Pro</li>
+      <li>MacBook Pro</li>
+      <li>Sony A7M4 with 24-105 lens</li>
+    </ul>
+  );
+}
+
+export default List;
